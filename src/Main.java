22dec8c10270a4b8b1c268d5b46d5375b2c96eb1
Chameera01 <chameera9019@gmail.com>
2023-12-28 21:02:23 +0800

    Add HashMap examples

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 
-import com.cham.learning.examples.ArrayListEx;
+import com.cham.learning.examples.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Learn & Practise Java Basics..!");
         System.out.println();
 
-        ArrayListEx.tryArrayList();
+        //ArrayListEx.tryArrayList();
+        HashMapEx.tryHashMap();
     }
 }
---
+++ src/com/cham/learning/examples/HashMapEx.java
@@ -0,0 +1,40 @@
+package com.cham.learning.examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx {
+
+    public static Map<String, Integer> hm1 = new HashMap<>();
+    public static Map<Person, String> personJob = new HashMap<>();
+    static Person p1 = new Person("Rasika",30);
+    static Person p2 = new Person("Sadun",35);
+
+    public static void tryHashMap() {
+
+        hm1.put("one", 1); //insertion
+        hm1.put("four", 4);
+        hm1.put("two", 2);
+        System.out.println("Is key \"four\" available? : " + hm1.containsKey("four"));
+        System.out.println("value of four is : " + hm1.get("four")); //Retrieval
+
+        hm1.remove("four"); //Remove
+        System.out.println("Is key \"four\" available? : " + hm1.containsKey("four")); //Availability
+        System.out.println("Is value \"4\" available? : " + hm1.containsValue(4));
+        System.out.println();
+
+        personJob.put(p1, "Developer");
+        personJob.put(p2, "Teacher");
+        System.out.println("Second person's job is : " + personJob.get(p2));
+    }
+
+    static class Person { //inner class
+        String name;
+        int age;
+        public Person(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+}
